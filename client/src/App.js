9c6bc9e01ee6d702293729e19086c497f7fe2fc4
Renato Jesus <orenatoaj@gmail.com>
2023-08-23 22:15:32 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Fib calculator version: 2</h1>
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">About us</Link>
+          </div>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
